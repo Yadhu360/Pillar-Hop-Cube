6caae9d028e017dc7413727d89351bc1fd767e45
Yedu <60286774+Yadhu360@users.noreply.github.com>
2025-12-12 04:36:45 +0530

    Add files via upload

---
+++ Assets/Scripts/PillarBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarBehaviour : MonoBehaviour {
+
+	public GameManager gameManager;
+	public GameObject cube; 
+	public int score=0;
+	Rigidbody cubeRB;
+	bool visited;
+
+	void Start(){
+		cubeRB = cube.GetComponent<Rigidbody>();
+		visited = false;
+	}
+
+	void OnCollisionStay(Collision other){
+		if(other.gameObject.tag.Equals("Cube")){
+			if(!visited){
+				if(Mathf.Approximately(cubeRB.linearVelocity.y,0) && Mathf.Approximately(cubeRB.linearVelocity.z,0)){
+					visited = true;
+					gameManager.scoreUp(score);
+				}
+			}
+		}
+	}
+
+
+}
